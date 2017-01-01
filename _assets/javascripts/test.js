d1a57512621083b94e123a9b8c2aa150809127de
enylund <nylund.eric@gmail.com>
2017-01-01 16:15:50 -0500

    resolves overlay at mobile

--- _assets/javascripts/test.js
+++ _assets/javascripts/test.js
@@ -1,16 +1,11 @@
 App.pageLoad.push(function() {
-  // $('.post__content a').mouseenter(function(){
-  //   $('body').css('background', 'red');
-  // });
-  // $('.post__content a').mouseleave(function(){
-  //   $('body').css('background', 'white');
-  // });
-  $('.circle').mouseenter(function(){
-    console.log("hi");
-    $('.homepage--overlay').addClass('active');
-  });
-  $('.circle').mouseleave(function(){
-    console.log("hi");
-    $('.homepage--overlay').removeClass('active');
-  });
+    $('.circle').mouseenter(function(){
+      console.log("hi");
+      $('.homepage--overlay').addClass('active');
+    });
+    $('.circle').mouseleave(function(){
+      console.log("hi");
+      $('.homepage--overlay').removeClass('active');
+    });
+
 });
